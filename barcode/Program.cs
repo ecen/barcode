fc44c2e1351b8833adf9008ee76096ff636ef6d4
ecen <ecen.cronzeton@gmail.com>
2021-04-27 08:25:42 +0200

    Initialized project.

---
+++ barcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace barcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
