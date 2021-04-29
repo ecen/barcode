3389f71a5c190b410ed6d4177671510589d0577d
ecen <ecen.cronzeton@gmail.com>
2021-04-29 11:58:24 +0200

    Added binary conversion function. Moved tests to its own project.

---
+++ barcode/barcode-test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using barcode;
+using NUnit.Framework;
+
+namespace barcode_test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ToBinaryStringSimple()
+        {
+            string s = "▍  ▍ ";
+            string expected = "10010";
+            Assert.AreEqual(BarcodeProgram.ToBinaryString(s), expected);
+        }
+
+        [Test]
+        public void ToBinaryStringNewline()
+        {
+            string s = "▍  ▍ \n";
+            string expected = "10010";
+            Assert.AreEqual(BarcodeProgram.ToBinaryString(s), expected);
+        }
+
+        [Test]
+        public void ToBinaryStringTestdata()
+        {
+            string s = "▍ ▍   ▍▍ ▍ ▍▍   ▍  ▍▍  ▍   ▍▍ ▍   ▍▍ ▍   ▍▍ ▍ ▍ ▍ ▍▍▍  ▍ ▍▍  ▍▍ ▍▍ ▍▍  ▍  ▍▍▍ ▍▍  ▍▍ ▍   ▍  ▍ ▍";
+            string expected = "10100011010110001001100100011010001101000110101010111001011001101101100100111011001101000100101";
+            Assert.AreEqual(BarcodeProgram.ToBinaryString(s), expected);
+        }
+    }
+}
---
+++ barcode/barcode/BarcodeProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace barcode
+{
+    public class BarcodeProgram
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = FileToBinaryLines(args[0]);
+            foreach (string line in lines)
+            {
+                Console.WriteLine($"{line}");
+            }
+        }
+
+        public static string[] FileToBinaryLines(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            string[] outputLines = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                outputLines[i] = ToBinaryString(lines[i]);
+            }
+            return outputLines;
+        }
+
+        public static string ToBinaryString(string barcode)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in barcode)
+            {
+                if (c == '▍') sb.Append('1');
+                else if (c == ' ') sb.Append('0');
+            }
+            return sb.ToString();
+        }
+    }
+}
